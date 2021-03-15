95a1b08595dba4bb0f7acb6765f8ddb5dbb40232
Alexander Fedorov <fedlogic@gmail.com>
2021-03-15 17:09:25 +1300

    Inteliji

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
+import Dialogs from "./Components/Dialogs/Dialogs";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div>Hello World!</div>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <div>Hello World! Бляяяяять</div>
+            </header>
+            <div className={'app-wrapper-content'}>
+                <Dialogs name='Напишу тут Хуету' />
+            </div>
+        </div>
+    );
 }
 
 export default App;

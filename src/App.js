a4145c64217af82cb9cba655dcc72c9c50a92e5e
Alexander Fedorov <>
2021-03-16 21:03:24 +1300

    Pushing crap to the rep

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import Header from "./Components/Header/Header";
 import Navbar from "./Components/Navbar/Navbar";
 import NumericInput from "./Components/NumericField/NumericField";
 import Profile from "./Components/Profile/Profile";
+import Dialogs from "./Components/Dialogs/Dialogs";
 
 const App = () => {
   return (
     <div className='appWrapper'>
         <Header />
         <Navbar />
-        <Profile />
+        <Dialogs />
+        {/*<Profile />*/}
         <NumericInput />
 
     </div>
---
+++ src/Components/Dialogs/Dialogs.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './Dialogs.module.css';
+
+const Dialogs = () => {
+    return (
+        <div className={styles.d}>
+    Dialogs
+        </div>
+    )
+}
+
+export default Dialogs;
+

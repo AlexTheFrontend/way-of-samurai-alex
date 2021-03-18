b0438851f99c1f9c55b25245bde331c3d514cba1
Alexander Fedorov <>
2021-03-18 19:53:55 +1300

    Lesson 23

--- src/Components/Dialogs/Dialogs.js
+++ src/Components/Dialogs/Dialogs.js
@@ -1,11 +1,43 @@
 import React from 'react';
 import styles from './Dialogs.module.css';
+import {NavLink} from "react-router-dom";
+
+const DialogItem = (props) => {
+    let path = "/dialogs/" + props.id;
+    return (
+        <div className={styles.dialog + ' ' + styles.active}>
+            <NavLink to={path}>{props.name}</NavLink>
+        </div>
+    )
+}
+
+const MessageItem = (props) => {
+    return (
+        <div className={styles.message}>{props.message}</div>
+    )
+}
 
 const Dialogs = (props) => {
     return (
+
         <div className={styles.d}>
-            {props.name} Dialogs
+            {/*{props.name} Dialogs*/}
+            <div className={styles.dialogItems}>
+                <DialogItem name={"Sergey"} id={"1"}/>
+                <DialogItem name={"Dennis"} id={"2"}/>
+                <DialogItem name={"Roman"} id={"3"}/>
+                <DialogItem name={"Nadya"} id={"4"}/>
+                <DialogItem name={"Dima"} id={"5"}/>
+                <DialogItem name={"Julia"} id={"6"}/>
+            </div>
+            <div className={styles.messages}>
+                <MessageItem message="Hello"/>
+                <MessageItem message="Sup!"/>
+                <MessageItem message="Bruuuuuuuu"/>
+            </div>
         </div>
+
+
     )
 }
 

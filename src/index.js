2168a6a4b87ae1e4d72669bc45678810a8c457d1
Alexander <fedlogic@gmail.com>
2021-05-11 15:15:55 +1200

    Adding breakpoints, colours for later usage

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import reportWebVitals from './reportWebVitals';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './Styles/index.css';
 import App from './App';
 import store from "./Redux/reduxStore";
 import {Provider} from "react-redux";

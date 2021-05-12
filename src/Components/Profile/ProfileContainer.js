6ef1e5008606005320e14559694f0c743a5bf900
Alexander <fedlogic@gmail.com>
2021-05-12 12:48:46 +1200

    l59: creating a class component for Profile.js, making AJAX queries to get some information about a user. Work with API.  Dumb logic to display only one user for now.

---
+++ src/Components/Profile/ProfileContainer.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Profile from "./Profile";
+import * as axios from "axios";
+import {connect} from "react-redux";
+import {setUserProfile} from "../../Redux/profileReducer";
+
+
+class ProfileContainer extends React.Component {
+
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
+            .then(response => {
+                this.props.setUserProfile(response.data);
+            })
+    }
+
+    render() {
+        return (
+            <Profile {...this.props} profile={this.props.profile} />
+        );
+    }
+}
+
+//returning an object
+let mapStateToProps = (state) => ({
+    profile: state.profilePage.profile
+});
+
+export default connect(mapStateToProps, {setUserProfile})(ProfileContainer);

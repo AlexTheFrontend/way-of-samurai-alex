a6d3c6461b22bbddcbe7ccd4abc02451d0057432
Alexander <fedlogic@gmail.com>
2021-06-02 16:57:02 +1200

    Login.js

---
+++ src/Components/Login/Login.js
@@ -0,0 +1,38 @@
+import React from "react";
+import {Field, reduxForm} from "redux-form";
+
+const LoginForm = (props) => {
+    return (
+        <form onSubmit={props.handleSubmit}>
+            <div>
+                <Field placeholder={"Login"} name={"login"} component={"input"}/>
+            </div>
+            <div>
+                <Field placeholder={"Password"} name={"password"} component={"input"}/>
+            </div>
+            <div>
+                <Field component={"input"} name={"rememberMe"} type={"checkbox"}/> remember me
+            </div>
+            <div>
+                <button>Log in</button>
+            </div>
+        </form>)
+}
+
+const LoginReduxForm = reduxForm({
+    // a unique name
+    form: 'login'
+})(LoginForm)
+
+const Login = () => {
+    const onSubmit = (formData) => {
+        console.log(formData)
+    }
+    return <div>
+        <h1>Login</h1>
+        <LoginReduxForm onSubmit={onSubmit} />
+
+    </div>
+}
+
+export default Login;

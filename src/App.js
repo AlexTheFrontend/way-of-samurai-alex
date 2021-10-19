63b417eb26441af451d3d681dd70f1d2a3fc18b5
Alexander <fedlogic@gmail.com>
2021-10-19 22:46:56 +1300

    Playing around with React.lazy and React.suspense

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React, {Component} from 'react'
+import React, {Component, Suspense} from 'react'
 import './App.css'
 import Navbar from "./Components/Navbar/Navbar";
-import ProfileContainer from "./Components/Profile/ProfileContainer";
+// import ProfileContainer from "./Components/Profile/ProfileContainer";
 import {BrowserRouter, Route} from "react-router-dom";
 import Music from "./Components/NewsMusicSettings/Music";
 import News from "./Components/NewsMusicSettings/News";
 import Settings from "./Components/NewsMusicSettings/Settings";
 import Friends from "./Components/Friends/Friends";
-import DialogsContainer from "./Components/Dialogs/DialogsContainer";
-import UsersContainer from "./Components/Users/UsersContainer";
+// import DialogsContainer from "./Components/Dialogs/DialogsContainer";
+// import UsersContainer from "./Components/Users/UsersContainer";
 import HeaderContainer from "./Components/Header/HeaderContainer";
 import Login from "./Components/Login/Login";
 import {connect, Provider} from "react-redux";
@@ -16,6 +16,11 @@ import {initialiseApp} from "./Redux/appReducer";
 import {compose} from "redux";
 import {withRouter} from "react-router";
 import store from "./Redux/reduxStore";
+import Preloader from "./Components/Common/Preloader/Preloader";
+
+const DialogsContainer = React.lazy(() => import('./Components/Dialogs/DialogsContainer'));
+const ProfileContainer = React.lazy(() => import('./Components/Profile/ProfileContainer'));
+const UsersContainer = React.lazy(() => import('./Components/Users/UsersContainer'));
 
 class App extends Component {
 
@@ -30,19 +35,21 @@ class App extends Component {
                 <HeaderContainer/>
                 <Navbar/>
                 <div className="appWrapperContent">
-                    {/*adding an optional URL parameter*/}
-                    <Route path="/profile/:userId?" render={() => <ProfileContainer/>}/>
-                    {/*the way to do it if defining on top*/}
-                    {/*with props.messages I am taking data from the level above*/}
-                    <Route path="/dialogs" render={() => <DialogsContainer/>}/>
-                    <Route path="/users" render={() => <UsersContainer/>}/>
-                    <Route path="/login" render={() => <Login/>}/>
+                    <Suspense fallback={"Loading... Please hold on, contect is coming"}>
+                        {/*adding an optional URL parameter*/}
+                        <Route path="/profile/:userId?" render={() => <ProfileContainer/>}/>
+                        {/*the way to do it if defining on top*/}
+                        {/*with props.messages I am taking data from the level above*/}
+                        <Route path="/dialogs" render={() => <DialogsContainer/>}/>
+                        <Route path="/users" render={() => <UsersContainer/>}/>
+                        <Route path="/login" render={() => <Login/>}/>
 
-                    {/* To be used later on */}
-                    <Route path="/news" render={() => <News/>}/>
-                    <Route path="/music" render={() => <Music/>}/>
-                    <Route path="/settings" render={() => <Settings/>}/>
-                    <Route path="/friends" render={() => <Friends/>}/>
+                        {/* To be used later on */}
+                        <Route path="/news" render={() => <News/>}/>
+                        <Route path="/music" render={() => <Music/>}/>
+                        <Route path="/settings" render={() => <Settings/>}/>
+                        <Route path="/friends" render={() => <Friends/>}/>
+                    </Suspense>
                 </div>
             </div>
         );
@@ -50,7 +57,7 @@ class App extends Component {
 }
 
 const mapStateToProps = (state) => ({
-  initialised: state.app.initialised
+    initialised: state.app.initialised
 })
 
 const AppContainer = compose(
@@ -60,9 +67,9 @@ const AppContainer = compose(
 const AlexSocialNetworkApp = (props) => {
     return <BrowserRouter>
         <Provider store={store}>
-            <AppContainer />
+            <AppContainer/>
         </Provider>
     </BrowserRouter>
 }
 
-export  default AlexSocialNetworkApp
+export default AlexSocialNetworkApp

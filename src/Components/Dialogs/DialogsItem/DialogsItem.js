ce87de171e244cbdbf36de71e222157b5652635f
Alexander <fedlogic@gmail.com>
2021-10-31 13:36:32 +1300

    Fixing rederice for profile pages from messages

--- src/Components/Dialogs/DialogsItem/DialogsItem.js
+++ src/Components/Dialogs/DialogsItem/DialogsItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {NavLink} from "react-router-dom";
 
 const DialogsItem = (props) => {
-    const path = "/dialogs/" + props.id;
+    const path = "/profile/" + props.id;
     return (
         <div>
             <NavLink to={path}>{props.name}</NavLink>
